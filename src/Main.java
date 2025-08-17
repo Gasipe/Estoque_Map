68c088dc3bc8e20ed42d12bf2034cb4dd6a3f765
Gasipe <gblsilvap@gmail.com>
2025-08-17 12:43:14 -0300

    começando a separar os métodos para melhor interpretação do código e manutenção

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,56 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        Map<Integer, Product> productMap = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        Map<Integer, Product> productMap = new HashMap<>();
+
+        int option;
+        do {
+            displayMenu();
+            System.out.println("Choose an option: ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 2 : System.out.println("listar todos produtos");
+                default: System.out.println("Invalid option! Please try again");
+            }
+        }
+        while(option != 3);
+
+        sc.close();
 
         System.out.print("How many products do you want to register?: ");
         int times = sc.nextInt();
 
-        for (int i = 0; i < times; i++) {
-            System.out.print("Enter the code of the product you wish to register: ");
-            int code = sc.nextInt();
+
+        //--------------Para exibir o resultado ---------------
+        for(Map.Entry<Integer, Product> entry : productMap.entrySet()) {
+            System.out.println("-------PRODUCT-------");
+            System.out.printf("Code: ", entry.getKey());
+            System.out.printf("Product: ", entry.getValue().getName());
+            System.out.printf("Price: %.2f" + entry.getValue().getPrice());
+            System.out.printf("Quantitie: ", entry.getValue().getQuantitie());
+            System.out.println("----------------------");
+
+        }
+
+    }
+    public static void displayMenu() {
+        System.out.println("\n ==== Product Management System ====");
+        System.out.println("1. Register new Products: ");
+        System.out.println("2. List all products: ");
+        System.out.println("3. Exit");
+    }
+
+    public static void registerProduct(Map<Integer, Product> productMap) {
+        //---------Criação do produto----------
+        System.out.print("Enter the code of the product you wish to register: ");
+        Scanner sc = new Scanner(System.in);
+        int code = sc.nextInt();
             sc.nextLine();
 
             System.out.print("What is the name of the product?: ");
@@ -30,19 +68,9 @@ public class Main {
             int quantitie = sc.nextInt();
             sc.nextLine();
 
-            Product product = new Product(name,price,quantitie);
+            Product product = new Product(name, price, quantitie);
             productMap.put(code, product);
 
-            }
-        for(Map.Entry<Integer, Product> entry : productMap.entrySet()) {
-            System.out.println("-------PRODUCT-------");
-            System.out.println("Code: " + entry.getKey());
-            System.out.println("Product: " + entry.getValue().getName());
-            System.out.printf("Price: %.2f\n", entry.getValue().getPrice());
-            System.out.println("Quantitie: " + entry.getValue().getQuantitie());
-            System.out.println("----------------------");
-
-        }
-
     }
+
 }
